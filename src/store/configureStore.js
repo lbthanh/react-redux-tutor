3deec880e4ba0981dd5a4a70d133e52d09fc5a75
Le Ba Thanh <lb.thanh@gloops.com>
2018-01-24 17:08:51 +0900

    production build

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,16 +1,6 @@
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-
-// Apply 'thunk' to middle to handle asysnchronous api 
-
-export default function configureStore(initialState) {
-    return createStore(
-        rootReducer,
-        initialState,
-        applyMiddleware(thunk, 
-                        reduxImmutableStateInvariant()
-                        )
-    );
+// dynamic import still were not supported by ES6, 'require' instead of 'import'
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./configureStore.prod');
+} else {
+    module.exports = require('./configureStore.dev');
 }
